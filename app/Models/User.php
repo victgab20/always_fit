99115305f4cecba5079b64bbce21798358c74a14
Victor Gabriel <victor.sousa@onearth.com.br>
2025-08-31 13:44:29 -0300

    feat: add relationship for User model to manage associated Trainings

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Domain\Training\Models\Training;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -32,6 +34,10 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+    public function trainings()
+    {
+        return $this->hasMany(Training::class);
+    }
 
     /**
      * Get the attributes that should be cast.
